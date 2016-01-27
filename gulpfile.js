0e055142de7a6fd80a748dabaddc98d2b54215a0
nmadd <nate@fratmusic.com>
2016-01-27 11:48:51 -0500

    Added Travis

--- gulpfile.js
+++ gulpfile.js
@@ -129,6 +129,12 @@ gulp.task('build', function () {
     }
 });
 
+gulp.task('end', ['testServerJSWithCoverage'], function() {
+  process.exit(0);
+});
+
+gulp.task('travis', ['lintJS','buildJS','buildCSS','testServerJSWithCoverage','end']);
+
 gulp.task('default', function () {
 
     gulp.start('build');
