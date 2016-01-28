a1c4ba6dbd49a1a6fc0df904e44f87ab5f2d7319
Alan <ajohnson88@gmail.com>
2016-01-28 09:22:34 -0500

    basic playerfactory created. Added controller to for home state.

--- browser/js/common/factories/playerFactory.js
+++ browser/js/common/factories/playerFactory.js
@@ -1,3 +1,60 @@
 app.factory('PlayerFactory', () => {
 
+    return {
+            play() {
+                youtubePlayer.playVideo();
+            },
+
+            pause() {
+                youtubePlayer.pauseVideo();
+            },
+
+            stop() {
+                youtubePlayer.stopVideo();
+            },
+
+            seekTo(seconds, seekAhead) {
+                youtubePlayer.seekTo(seconds, seekAhead);
+            },
+
+            next() {
+                youtubePlayer.nextVideo();
+            },
+
+            previous() {
+                youtubePlayer.previousVideo();
+            },
+
+            mute() {
+                youtubePlayer.mute();
+            },
+
+            unMute() {
+                youtubePlayer.unMute();
+            },
+
+            isMuted() {
+                youtubePlayer.isMuted();
+            },
+
+            setVolume(volume) {
+                youtubePlayer.setVolume();
+            },
+
+            getVolume() {
+                youtubePlayer.getVolume();
+            },
+
+            //Playback status:
+
+            getVideoLoadedFraction() {
+                youtubePlayer.getVideoLoadedFraction();
+            },
+
+            getPlayerState() {
+                youtubePlayer.getPlayerState();
+            }
+
+    }
+
 })
