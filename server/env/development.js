14fee2288f6e90ac2f271ad8df5b83aeb3c591a5
Sean Sarkar <wearymachine@Seans-MacBook-Air.local>
2016-01-28 17:54:20 -0500

    got fb auth working

--- server/env/development.js
+++ server/env/development.js
@@ -7,9 +7,9 @@ module.exports = {
     "callbackUrl": "INSERT_TWITTER_CALLBACK_HERE"
   },
   "FACEBOOK": {
-    "clientID": "INSERT_FACEBOOK_CLIENTID_HERE",
-    "clientSecret": "INSERT_FACEBOOK_CLIENT_SECRET_HERE",
-    "callbackURL": "INSERT_FACEBOOK_CALLBACK_HERE"
+    "clientID": "537254919776115",
+    "clientSecret": "51283755608fced098f015dc981965df",
+    "callbackURL": "http://localhost:1337/auth/facebook/callback"
   },
   "GOOGLE": {
     "clientID": "226144017822-783klujt9l3hm9696vphhb8u5qck1le8.apps.googleusercontent.com",
