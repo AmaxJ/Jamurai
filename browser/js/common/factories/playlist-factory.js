f883ab5ef5f291ad771d31b165adb750bb85db2e
Alan <ajohnson88@gmail.com>
2016-01-28 15:24:52 -0500

    modified playlist voting

--- browser/js/common/factories/playlist-factory.js
+++ browser/js/common/factories/playlist-factory.js
@@ -1,18 +1,33 @@
 app.factory('PlaylistFactory', function($http) {
-	var factory = {};
-	factory.getAllSongs = function () {
+
+    var factory = {};
+    var playlist = [];
+
+	factory.populateSongs = function () {
 		return $http.get('/api/songs/')
 		.then(function(songs){
             songs.data.forEach(function(song) {
                 song.voteValue = 0;
             });
-			return songs.data;
+            playlist = songs.data;
 		});
 	};
 
-    factory.vote = function($event, song, downvote) {
+    factory.sort = function() {
+        playlist.sort(function(a, b) {
+            return b.voteValue - a.voteValue;
+        });
+    };
+
+    factory.vote = function($event, song, vote) {
         $event.stopPropagation();
-        downvote ? song.voteValue -= 1 : song.voteValue += 1;
+        if (vote === 'up') song.voteValue += 1;
+        else if (vote === 'down') song.voteValue -= 1;
+        factory.sort();
+    };
+
+    factory.getPlaylist = function() {
+        return playlist;
     };
 
 	return factory;
