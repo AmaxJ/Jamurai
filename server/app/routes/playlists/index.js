998b95dbda55ca7701ae3736f0cb8e379908c7ca
Alan <ajohnson88@gmail.com>
2016-01-30 17:34:02 -0500

    added playlist routes

--- server/app/routes/playlists/index.js
+++ server/app/routes/playlists/index.js
@@ -1,22 +1,42 @@
-'use strict';
-var router = require('express').Router();
-var Playlist = require('mongoose').model('Playlist');
+let router = require('express').Router();
+let Playlist = require('mongoose').model('Playlist');
 module.exports = router;
 
 router.route('/')
-    .get(function(req,res,next){
+    .get((req, res, next) => {
         Playlist.find({})
-        .populate('songs')
-        .exec()
-        .then(function(playlist){
-            res.json(playlist);
-        })
-        .then(null,next);
+            .populate('songs')
+            .exec()
+            .then(playlist => {
+                res.json(playlist);
+            })
+            .then(null, next);
     })
-    .post(function(req,res,next){
+    .post((req, res, next) => {
         Playlist.create(req.body)
-        .then(function(playlist){
-            res.status(201).json(playlist);
-        })
-        .then(null,next);
+            .then(playlist => {
+                res.status(201).json(playlist);
+            })
+            .then(null, next);
+    });
+
+router.route('/:playlistId')
+    .get((req, res, next) => {
+        Playlist.findById(req.params.playlistId)
+            .populate('songs')
+            .exec()
+            .then(playlist => {
+                res.status(200).json(playlist);
+            })
+            .then(null, next);
     })
+    .put((req, res, next) => {
+        Playlist.findById(req.params.playlistId)
+            .then(playlist => {
+                return playlist.addSong(req.body.song._id);
+            })
+            .then( playlist => {
+                res.status(204).json(playlist);
+            })
+            .then(null, next);
+    });
