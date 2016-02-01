47d7ac71dd94359bb94fd12a6c103edd5f7b3704
Alan <ajohnson88@gmail.com>
2016-02-01 18:46:26 -0500

    changed room creation route to create playlist as well. Modified seed file. Changed 'playlists' field from an array to a single playlist object 'playlist'

--- server/app/routes/rooms/index.js
+++ server/app/routes/rooms/index.js
@@ -1,67 +1,74 @@
 'use strict';
 var router = require('express').Router();
 var Room = require('mongoose').model('Room');
+var Playlist = require('mongoose').model('Playlist')
 module.exports = router;
 
 router.route('/')
-	.get(function(req,res,next){
-		Room.find({})
-		.populate('users')
-		.populate('creator')
-        .populate('playlists')
-        .populate({
-            path: 'playlists',
-            model: 'Playlist',
-            populate : {
-                path : 'songs',
-                model : 'Song'
-            }
-        })
-		.exec()
-		.then(function(rooms){
-			res.json(rooms);
-		})
-		.then(null,next);
-	})
-	.post(function(req,res,next){
-		Room.create(req.body)
-		.then(function(room){
-			res.status(201).json(room);
-		})
-		.then(null,next);
-	})
+    .get((req, res, next) => {
+        Room.find({})
+            .populate('users')
+            .populate('creator')
+            .populate({
+                path: 'playlist',
+                model: 'Playlist',
+                populate: {
+                    path: 'songs',
+                    model: 'Song'
+                }
+            })
+            .exec()
+            .then(rooms => {
+                res.json(rooms);
+            })
+            .then(null, next);
+    })
+    .post((req, res, next) => {
+        let playlist;
+        Playlist.create({})
+            .then(newPlaylist => {
+                req.body.playlist = newPlaylist;
+                return Room.create(req.body)
+            })
+            .then(room => {
+                res.status(201).json(room);
+            })
+            .then(null, next);
+    })
 
 router.route('/:roomId')
-	.get(function(req,res,next){
-		Room.findById(req.params.roomId)
-		.populate('songs')
-		.populate('users')
-        .populate('creator')
-        .populate({
-            path: 'playlists',
-            model: 'Playlist',
-            populate : {
-                path : 'songs',
-                model : 'Song'
-            }
-        })
-        .then(function(room){
-            res.json(room);
-        })
-        .then(null,next);
-	})
-	.put(function(req,res,next){
-		Room.findByIdAndUpdate(req.params.roomId,req.body, {new: true})
-		.then(function(room){
-			res.status(204).json(room);
-		})
-		.then(null,next);
-	})
-	.delete(function(req,res,next){
-		Room.findByIdAndRemove(req.params.roomId)
-		.exec()
-		.then(function(room){
-			res.status(204).end();
-		})
-		.then(null,next);
-	})
+    .get((req, res, next) => {
+        Room.findById(req.params.roomId)
+            .populate('songs')
+            .populate('users')
+            .populate('creator')
+            .populate({
+                path: 'playlists',
+                model: 'Playlist',
+                populate: {
+                    path: 'songs',
+                    model: 'Song'
+                }
+            })
+            .then(room => {
+                res.json(room);
+            })
+            .then(null, next);
+    })
+    .put((req, res, next) => {
+        Room.findByIdAndUpdate(req.params.roomId, req.body, {
+                new: true
+            })
+            .then(room => {
+                res.status(204).json(room);
+            })
+            .then(null, next);
+    })
+    .delete((req, res, next) => {
+        Room.findByIdAndRemove(req.params.roomId)
+            .exec()
+            .then(room => {
+                res.status(204).end();
+            })
+            .then(null, next);
+    });
