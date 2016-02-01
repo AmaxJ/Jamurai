26824d54a73728d54c272711be57fcfe209c87a4
Alan Johnson <Ajohnson88@gmail.com>
2016-02-01 10:44:21 -0500

    Revert "Added lobby view with wired-up room states; also added some seed rooms"

--- browser/js/common/directives/room-creation/room-creation-directive.js
+++ browser/js/common/directives/room-creation/room-creation-directive.js
@@ -4,26 +4,18 @@ app.directive('createRoom', function(){
 		templateUrl: 'js/common/directives/room-creation/room-creation-template.html',
 		controller: 'RoomFormCtrl'
 	}
-}).controller('RoomFormCtrl', function($scope, RoomFactory, $state){
+})
 
+app.controller('RoomFormCtrl', function($scope, RoomFactory){
 	$scope.newRoom = {
-		//creator field just for testing... remove later
-		//normally will be: user._id
-		creator: "56ab95583c716b49114954ec",
-        name: null,
-        privacy: null,
-        location: null,
-        ambassadors: []
+        roomName: null,
+        roomPrivacy: null
     }
-
 	$scope.getRoomState = RoomFactory.getRoomState;
 	$scope.showForm = RoomFactory.showForm;
-	$scope.createNewRoom = () => {
+	$scope.createNewRoom = function() {
 			RoomFactory.createNewRoom($scope.newRoom)
-			.then((newRoom) => {
-				$state.go('room', {roomId: newRoom._id});
-			})
-			.then(() => {
+			.then(function(){
 				$scope.newRoom = {
 			        name: null,
 			        privacy: null,
