09a61b61f6a8cb1d8595211cfefda1ea73473ce0
Joe Alves <joe@fullstackacademy.com>
2016-02-01 17:05:38 -0500

    Wipe

--- browser/js/submit-song/submit-song-state.js
+++
@@ -1,6 +0,0 @@
-app.config(function($stateProvider) {
-    $stateProvider.state('submit-song', {
-        url: '/submit-song',
-        templateUrl: 'js/submit-song/submit-song-template.html'
-    });
-})
