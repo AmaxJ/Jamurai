a22fc52162823faebd40c80288a24d51c20ce321
Alan <ajohnson88@gmail.com>
2016-02-02 17:20:59 -0500

    Added songdata model

---
+++ server/db/models/songData.js
@@ -0,0 +1,40 @@
+'use strict';
+var mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+
+    playlist: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Playlist"
+    },
+
+    song: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Song"
+    },
+
+    upVotes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }],
+
+    downVotes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }],
+
+    total : {
+        type: Number,
+        default: 0
+    }
+
+});
+
+schema.methods({
+    calculateValue : function() {
+        this.total = this.upVotes - this.downVotes;
+        return this.save();
+    }
+});
+
+mongoose.model('SongData', schema);
