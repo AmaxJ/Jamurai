8beb7b26bc0ac4e0a0f9ef67d09a2474c3fc2e5d
Alan <ajohnson88@gmail.com>
2016-02-02 15:09:04 -0500

    init songdata object on playlist creation

--- server/db/models/playlist.js
+++ server/db/models/playlist.js
@@ -2,12 +2,12 @@
 var mongoose = require('mongoose');
 
 var schema = new mongoose.Schema({
-    songs : [{
+    songs: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "Song"
     }],
 
-    songData : {
+    songData: {
         type: mongoose.Schema.Types.Mixed
     }
 });
@@ -15,16 +15,28 @@ var schema = new mongoose.Schema({
 schema.method({
     addSong: function(songId) {
         this.songs.addToSet(songId);
-        if (!this.songData) {this.songData = {}}
+        if (!this.songData) {
+            this.songData = {}
+        }
         this.songData[songId] = 0;
+        this.markModified('songData');
         return this.save();
     },
-    updateSongValue : function(songId, total) {
+    updateSongValue: function(songId, total) {
         if (!(songId in this.songData)) return;
         this.songData[songId] = total;
         this.markModified('songData');
         return this.save();
     }
+});
+
+schema.post('init', function(doc) {
+    doc.songData = {};
+    doc.songs.forEach(function(songId) {
+        doc.songData[songId] = 0;
+    });
+    doc.markModified('songData')
+    doc.save();
 });
 
 mongoose.model('Playlist', schema);
