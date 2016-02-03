1755e71cb86d0ed7a441f8b81d8dfb827f5bd02b
Alan <ajohnson88@gmail.com>
2016-02-03 11:53:38 -0500

    added songdata methods

--- server/db/models/songData.js
+++ server/db/models/songData.js
@@ -32,7 +32,27 @@ var schema = new mongoose.Schema({
 
 schema.method({
     calculateValue : function() {
-        this.total = this.upVotes - this.downVotes;
+        this.total = this.upVotes.length - this.downVotes.length;
+        return this.save();
+    },
+    vote : function(userId, vote) { //vote = 'up' or 'down'
+        if (!userId || !vote) return;
+        if (this.upVotes.indexOf(userId) > -1) return;
+        if (this.downVotes.indexOf(userId) > -1) return;
+        if (vote === 'up') {
+            this.upvotes.push(userId);
+        } else if (vote === 'down') {
+            this.downVotes.push(userId);
+        }
+        return this.save();
+    },
+    removeVote: function(userId, vote) {
+        if (!userId) return;
+        if (vote) {
+            var voteProp = vote + "Votes";
+            var userIndex = this[voteProp].indexOf(userId);
+            this[voteProp].splice(userIndex, 1);
+        }
         return this.save();
     }
 });
