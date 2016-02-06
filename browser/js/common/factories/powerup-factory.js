a9c99f5bb694fa47501d8940ff314aedb972c60c
nmadd <nate@fratmusic.com>
2016-02-06 16:35:23 -0500

    Added a bunch more power ups

--- browser/js/common/factories/powerup-factory.js
+++ browser/js/common/factories/powerup-factory.js
@@ -2,22 +2,50 @@ app.factory('PowerupFactory', (PlaylistFactory, $rootScope, SocketFactory) => {
     var factory = {};
     var socket = SocketFactory.getSocket();
     var powerUps = {
-        'superVote': superVote,
-        'downvoteBomb': downvoteBomb,
-        'deathStars' : deathStars
+        'swordOfCertainDeath': swordOfCertainDeath,
+        'deathStars' : deathStars,
+        'swordOfHonor' : swordOfHonor,
+        'swordOfDisgrace' : swordOfDisgrace,
+        'swordOfHolyLegend' : swordOfHolyLegend,
+        'swordOfUncertainty' : swordOfUncertainty
     }
 
-    function superVote () {
+    function swordOfHonor () {
         PlaylistFactory.setUpvoteAmount(10);
         $rootScope.$on('upvote', ()=> {
             PlaylistFactory.setUpvoteAmount(1);
         })
     }
 
-    function downvoteBomb () {
+    function swordOfHolyLegend () {
+        PlaylistFactory.setUpvoteAmount(100);
+        $rootScope.$on('upvote', ()=> {
+            PlaylistFactory.setUpvoteAmount(1);
+        })
+    }
+
+    function swordOfCertainDeath () {
         PlaylistFactory.setDownvoteAmount(-100);
         $rootScope.$on('downvote', ()=> {
             PlaylistFactory.setDownvoteAmount(-1);
+        })
+    }
+
+    function swordOfDisgrace () {
+        PlaylistFactory.setDownvoteAmount(-10);
+        $rootScope.$on('downvote', ()=> {
+            PlaylistFactory.setDownvoteAmount(-1);
+        })
+    }
+
+    function swordOfUncertainty () {
+        var posOrNeg = [-10, 10];
+        var zeroOrOne = Math.floor(Math.random()*2);
+        var multiplier = posOrNeg[zeroOrOne];
+        var randomNum = Math.floor(Math.random()*multiplier);
+        PlaylistFactory.setUpvoteAmount(randomNum);
+        $rootScope.$on('upvote', ()=> {
+            PlaylistFactory.setUpvoteAmount(1);
         })
     }
 
--- server/db/models/powerupData.js
+++ server/db/models/powerupData.js
@@ -18,7 +18,7 @@ var schema = new mongoose.Schema({
 
 schema.method({
     addPowerup : function() {
-        var powerUps =['superVote', 'downvoteBomb', 'deathStars'];
+        var powerUps =['swordOfHonor', 'swordOfCertainDeath', 'deathStars', 'swordOfDisgrace', 'swordOfHolyLegend', 'swordOfUncertainty'];
         var randomPowerUp = powerUps[Math.floor(Math.random()*powerUps.length)];
         this.powerups.push(randomPowerUp);
         return this.save();
