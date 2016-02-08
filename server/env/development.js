4832d575c5095e206625458d8f6b1f921bcf1d9f
nmadd <nate@fratmusic.com>
2016-02-08 14:52:15 -0500

    Changed local environment variables

--- server/env/development.js
+++ server/env/development.js
@@ -1,6 +1,6 @@
 module.exports = {
   "DATABASE_URI": "mongodb://localhost:27017/capstone",
-  "SESSION_SECRET": "jamuraininjawarriors",
+  "SESSION_SECRET": "Optimus Prime is my real dad",
   "TWITTER": {
     "consumerKey": "INSERT_TWITTER_CONSUMER_KEY_HERE",
     "consumerSecret": "INSERT_TWITTER_CONSUMER_SECRET_HERE",
