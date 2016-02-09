cb37a01785e91e84be8f8b87cc83562cfc6ff989
Alan <ajohnson88@gmail.com>
2016-02-09 14:14:16 -0500

    added icons

--- server/app/configure/static-middleware.js
+++ server/app/configure/static-middleware.js
@@ -10,12 +10,14 @@ module.exports = function (app) {
     var npmPath = path.join(root, './node_modules');
     var publicPath = path.join(root, './public');
     var browserPath = path.join(root, './browser');
-    var bowerPath = path.join(root,'./bower_components')
+    var bowerPath = path.join(root,'./bower_components');
+    var imgPath = path.join(root, './static');
 
     app.use(favicon(app.getValue('faviconPath')));
     app.use(express.static(npmPath));
     app.use(express.static(publicPath));
     app.use(express.static(browserPath));
     app.use(express.static(bowerPath));
+    app.use(express.static(imgPath));
 
 };
