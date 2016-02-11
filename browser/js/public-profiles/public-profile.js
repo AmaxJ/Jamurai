b952d3b63e71348a2ab1f354c699c56cecf05a56
john-mcgowan1992 <mcgowanj92@gmail.com>
2016-02-11 17:45:08 -0500

    added powerups to resolve block

--- browser/js/public-profiles/public-profile.js
+++ browser/js/public-profiles/public-profile.js
@@ -9,38 +9,39 @@ app.config(function ($stateProvider) {
 						.then((user) => {
 							return user;
 						});
+			},
+			userPowerups: (publicUser) => {
+				let powerUpIcons = {
+				    'Chopsticks of Plenty': '/food.svg',
+				    'Sword of Ultimate Shame': '/twoswords.svg',
+				    'Daggers of Disdain': '/daggerSolid.svg',
+				    'Katana of Disgrace': '/sword.svg',
+				    'Enlightened Blessing': '/discipline.svg',
+				    'Sword of Uncertainty': '/yinyang.svg',
+				    'Poison Darts': '/darts.svg',
+				    'The Last Jamurai': '/helmet.svg'
+				}
+
+				let formatPowerUps = powerUpObj => {
+			        return powerUpObj.map(powerup => {
+			            let pwrUp = {};
+			            pwrUp.name = powerup;
+			            pwrUp.icon = powerUpIcons[powerup];
+			            return pwrUp;
+			        });
+			    }
+
+				UserFactory.getPowerupsByUser(publicUser._id)
+				.then(powerups => {
+					return formatPowerUps(powerups);
+				})
 			}
 		}
 	})
 })
-.controller('PublicProfileCtrl', function($scope, publicUser, UserFactory){
-
-	let formatPowerUps = powerUpObj => {
-        return powerUpObj.map(powerup => {
-            let pwrUp = {};
-            pwrUp.name = powerup;
-            pwrUp.icon = powerUpIcons[powerup];
-            return pwrUp;
-        });
-    }
-
-	let powerUpIcons = {
-	    'Chopsticks of Plenty': '/food.svg',
-	    'Sword of Ultimate Shame': '/twoswords.svg',
-	    'Daggers of Disdain': '/daggerSolid.svg',
-	    'Katana of Disgrace': '/sword.svg',
-	    'Enlightened Blessing': '/discipline.svg',
-	    'Sword of Uncertainty': '/yinyang.svg',
-	    'Poison Darts': '/darts.svg',
-	    'The Last Jamurai': '/helmet.svg'
-	}
+.controller('PublicProfileCtrl', function($scope, publicUser, UserFactory, userPowerups){
 
 	$scope.publicUser = publicUser;
-	$scope.userPowerups;
-
-	UserFactory.getPowerupsByUser(publicUser._id)
-	.then(powerups => {
-		$scope.userPowerups = formatPowerUps(powerups);
-	})
+	$scope.userPowerups = userPowerups;
 
 });
