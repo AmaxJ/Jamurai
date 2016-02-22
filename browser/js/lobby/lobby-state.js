b9bc25a203947c34a53efbf3e60e4fe6210c44a5
Alan <ajohnson88@gmail.com>
2016-02-22 11:32:36 -0500

    removed geolocation stuff

--- browser/js/lobby/lobby-state.js
+++ browser/js/lobby/lobby-state.js
@@ -1,29 +1,21 @@
-let geo = navigator.geolocation;
-
 app.config($stateProvider => {
     $stateProvider.state('lobby', {
         url: '/lobby',
         templateUrl: 'js/lobby/lobby.html',
         controller: 'LobbyCtrl',
         resolve: {
-        	user: (AuthService) => {
-        		return AuthService.getLoggedInUser()
-        				// .then(function(user){
-                        //     geo.getCurrentPosition(function(position){
-                        //         console.log('poz',position);
-                        //         var coords = [position.coords.latitude,position.coords.longitude];
-                        //         return UserFactory.updateUser(user._id,{coordinates: coords})
-
-                        //     })
-                        // })
+        	user(AuthService) {
+        		return AuthService.getLoggedInUser();
         	},
-            rooms: (RoomFactory) => {
+            rooms(RoomFactory) {
                 return RoomFactory.getAllRooms();
             }
         }
     })
 
-}).controller('LobbyCtrl', ($scope, user, rooms) => {
+})
+
+app.controller('LobbyCtrl', ($scope, user, rooms) => {
     $scope.user = user;
     $scope.rooms = rooms;
 
