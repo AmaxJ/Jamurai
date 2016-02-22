8dfe018fa2dc8be4db6779fd9ae5448bace48fe5
Alan <ajohnson88@gmail.com>
2016-02-22 11:37:08 -0500

    small cleanup

--- browser/js/login/login.js
+++ browser/js/login/login.js
@@ -1,26 +1,22 @@
 app.config($stateProvider => {
-
     $stateProvider.state('login', {
         url: '/login',
         templateUrl: 'js/login/login.html',
         controller: 'LoginCtrl'
     });
-
 });
 
 app.controller('LoginCtrl', ($scope, AuthService, $state) => {
-
     $scope.login = {};
     $scope.error = null;
-
-    $scope.sendLogin = (loginInfo) => {
-
+    $scope.sendLogin = loginInfo => {
         $scope.error = null;
-        
-        AuthService.login(loginInfo).then(() => {
-            $state.go('lobby');
-        }).catch(() => {
-            $scope.error = 'Invalid login credentials.';
-        });
+        AuthService.login(loginInfo)
+            .then(() => {
+                $state.go('lobby');
+            })
+            .catch(() => {
+                $scope.error = 'Invalid login credentials.';
+            });
     };
 });
