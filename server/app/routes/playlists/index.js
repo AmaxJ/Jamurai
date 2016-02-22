7f4e5790a6526556e8117db7c5393551f3db8f90
Alan <ajohnson88@gmail.com>
2016-02-22 11:07:05 -0500

    removed whitespace

--- server/app/routes/playlists/index.js
+++ server/app/routes/playlists/index.js
@@ -16,14 +16,14 @@ router.route('/')
                 }
             })
             .exec()
-            .then( playlist => {
+            .then(playlist => {
                 res.json(playlist);
             })
             .then(null, next);
     })
     .post((req, res, next) => {
         Playlist.create(req.body)
-            .then( playlist => {
+            .then(playlist => {
                 res.status(201).json(playlist);
             })
             .then(null, next);
@@ -41,17 +41,17 @@ router.route('/:playlistId')
                 }
             })
             .exec()
-            .then( playlist => {
+            .then(playlist => {
                 res.status(200).json(playlist);
             })
             .then(null, next);
     })
     .put((req, res, next) => {
         Playlist.findById(req.params.playlistId)
-            .then( playlist => {
+            .then(playlist => {
                 return playlist.addSong(req.body.song._id, req.body.user);
             })
-            .then( playlist => {
+            .then(playlist => {
                 res.status(204).json(playlist);
             })
             .then(null, next);
@@ -63,7 +63,7 @@ router.route('/:playlistId/score')
             .then(playlist => {
                 return playlist.updateSongValue(req.body.song._id, req.body.songValue);
             })
-            .then( playlist => {
+            .then(playlist => {
                 res.status(204).json(playlist);
             })
             .then(null, next);
@@ -71,7 +71,7 @@ router.route('/:playlistId/score')
 
 router.route('/:playlistId/songData')
     .get((req, res, next) => {
-        SongData.find({playlist: req.params.playlistId})
+        SongData.find({ playlist: req.params.playlistId })
             .populate('song')
             .then(songDataObjs => {
                 res.json(songDataObjs);
@@ -85,7 +85,7 @@ router.route('/:playlistId/:songId')
             .then(playlist => {
                 return playlist.removeSong(req.params.songId);
             })
-            .then( playlist => {
+            .then(playlist => {
                 res.status(204).json(playlist);
             })
             .then(null, next);
