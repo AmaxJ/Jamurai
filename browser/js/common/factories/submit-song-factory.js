b196f2dbae96ab000bc26190533a7ae892a5ae20
Alan <ajohnson88@gmail.com>
2016-02-22 12:49:20 -0500

    submit song factory clean

--- browser/js/common/factories/submit-song-factory.js
+++ browser/js/common/factories/submit-song-factory.js
@@ -1,30 +1,27 @@
-app.factory('SubmitSongFactory', ($http)=>{
-	let factory = {};
+app.factory('SubmitSongFactory', $http => {
 
-	let searchResults;
-	factory.searchYoutube = (searchParams)=>{
-		return $http({
-			method: 'GET',
-			url: 'https://www.googleapis.com/youtube/v3/search',
-			params: {
-				q: searchParams,
-				part: 'snippet',
-				maxResults: 25,
-				kind: 'video',
-				key:  'AIzaSyBP4BcLHPabkcqMhcvSBhQ93WaCrQzMk1A'
-			}
-		})
-		.then((response)=>{
-			searchResults  = response.data.items
-		})
-	}
+    let factory = {};
 
-	factory.getSearchResults = () => {
-		return searchResults;
-	}
-	return factory;
+    let searchResults;
+
+    factory.searchYoutube = searchParams => {
+        return $http({
+                method: 'GET',
+                url: 'https://www.googleapis.com/youtube/v3/search',
+                params: {
+                    q: searchParams,
+                    part: 'snippet',
+                    maxResults: 25,
+                    kind: 'video',
+                    key: 'AIzaSyBP4BcLHPabkcqMhcvSBhQ93WaCrQzMk1A'
+                }
+            })
+            .then(response => {
+                searchResults = response.data.items;
+            });
+    }
+
+    factory.getSearchResults = () => searchResults;
+
+    return factory;
 });
-
-
-
-
