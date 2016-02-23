e40ac1a00658f5b5d44895994b8d22cfba9504f1
john-mcgowan1992 <mcgowanj92@gmail.com>
2016-02-23 17:17:18 -0500

    testing randomizing function that weighs powerups differently

--- server/db/models/powerupData.js
+++ server/db/models/powerupData.js
@@ -1,7 +1,7 @@
 'use strict'
-var mongoose = require('mongoose');
+let mongoose = require('mongoose');
 
-var schema = new mongoose.Schema({
+let schema = new mongoose.Schema({
     user : {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
@@ -18,13 +18,41 @@ var schema = new mongoose.Schema({
 
 schema.method({
     addPowerup : function() {
-        var powerUps =['Lotus of Generosity', 'Sword of Ultimate Shame', 'Daggers of Disdain', 'Katana of Disgrace', 'Enlightened Blessing', 'Sword of Uncertainty', 'Poison Darts', 'The Last Jamurai'];
-        var randomPowerUp = powerUps[Math.floor(Math.random()*powerUps.length)];
-        this.powerups.push(randomPowerUp);
+        let powerUps =['Lotus of Generosity', 'Sword of Ultimate Shame', 'Daggers of Disdain', 'Katana of Disgrace', 'Enlightened Blessing', 'Sword of Uncertainty', 'Poison Darts', 'The Last Jamurai'];
+        
+        let randomEngine = () => {
+            let rando = +Math.floor(Math.random()*861);
+            console.log('our number:', rando);
+            if (rando >= 0 && rando < 21) {
+                return 'The Last Jamurai';
+            }
+            else if (rando > 20 && rando < 91) {
+                return 'Sword of Ultimate Shame';
+            }
+            else if (rando > 90 && rando < 241) {
+                return 'Lotus of Generosity';
+            }
+            else if (rando > 240 && rando < 341) {
+                return 'Daggers of Disdain';
+            }
+            else if (rando > 340 && rando < 451) {
+                return 'Katana of Disgrace';
+            }
+            else if (rando > 450 && rando < 621) {
+                return 'Enlightened Blessing';
+            }
+            else if (rando > 620 && rando < 761) {
+                return 'Sword of Uncertainty';
+            }
+            else if (rando > 760 && rando < 861) {
+                return 'Poison Darts'
+            }
+        }
+        this.powerups.push(randomEngine());
         return this.save();
     },
     usePowerup: function(powerUp) {
-        var powerIndex = this.powerups.indexOf(powerUp);
+        let powerIndex = this.powerups.indexOf(powerUp);
         this.powerups.splice(powerIndex, 1);
         return this.save();
     }
